6ecf23c7b3b156e61b05e1ad627a920b0db8bd79
Illucious <visheshgarg570@gmail.com>
2023-05-25 19:35:52 +0530

    initial Commit

---
+++ RESTapi_audio_CRUD/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RESTapi_audio_CRUD project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RESTapi_audio_CRUD.settings')
+
+application = get_asgi_application()
